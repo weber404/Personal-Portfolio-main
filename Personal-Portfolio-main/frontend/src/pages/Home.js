946b11dbb0fc8dfa922390d2b95d52d064787056
Vishwas Saxena <185520121+weber404@users.noreply.github.com>
2025-10-18 22:46:41 +0530

    Update resume link to new file name

--- Personal-Portfolio-main/frontend/src/pages/Home.js
+++ Personal-Portfolio-main/frontend/src/pages/Home.js
@@ -137,7 +137,7 @@ const Home = () => {
                   <ArrowRight className="ml-2 h-5 w-5 group-hover:translate-x-1 transition-transform duration-300" />
                 </Button>
               </Link>
-              < a href='Vishwas Saxena-Front-End Developer-Resume.pdf' download >
+              < a href='Vishwas_Saxena_Full-Stack Developer_Resume.pdf' download >
               <Button 
                 variant="outline" 
                 size="lg"
